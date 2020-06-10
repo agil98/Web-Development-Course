6c36fad7256ae39800fa80cc944c664e162e1435
Alexandra <alexandragil98@outlook.com>
2020-06-09 19:28:18 -0700

    Created sign up page, modified backend for login and moved user model into application.py

--- Book Review Web Site/application.py
+++ Book Review Web Site/application.py
@@ -2,9 +2,19 @@ import os
 
 from flask import Flask, render_template, jsonify, request, session
 from flask_session import Session
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, String, Integer, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 from models import *
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(db.Integer, primary_key = True)
+    name = Column(db.String, nullable = False)
+    username = Column(db.String, unique = True, nullable = False)
+    password = Column(db.String, nullable = False)
 
 app = Flask(__name__)
 
@@ -15,25 +25,42 @@ if not os.getenv("DATABASE_URL"):
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
 
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
+Session()
+Base.metadata.create_all(engine)
 
 @app.route("/")
 def index():
-    return render_template("login.html")
+    #return render_template("login.html")
+    return render_template("signup.html")
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     input_username = request.form.get('username')
     input_password = request.form.get('password')
-    user = db.execute("SELECT id FROM users WHERE username = :username AND password = :password", {"username": input_username, "password": input_password}).fetchone()
+    user = db.query(User).filter(User.username==input_username and User.password==input_password).first()
     if user is None:
         return render_template("error.html", message="Could not find that user.")
     else:
         return render_template("success.html")
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+
+    input_name = request.form.get('name')
+    input_username = request.form.get('username')
+    input_password = request.form.get('password')
+    user = db.query(User).filter(User.username==input_username).first()
+    if user is None:
+        user = User(name = input_name, username = input_username, password = input_password)
+        db.add(user)
+        db.commit()
+        return render_template("success.html")
+    else:
+        return render_template("error.html", message="Select a new username")
 
 @app.route("/success", methods=["POST"])
 def success():
