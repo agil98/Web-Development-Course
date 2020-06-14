c62b1776df5f0751ba661f3a2cdea76541156ede
Alexandra <alexandragil98@outlook.com>
2020-06-14 15:30:09 -0700

    Partial commit so Tiger can see it

--- Book Review Web Site/application.py
+++ Book Review Web Site/application.py
@@ -6,7 +6,6 @@ from sqlalchemy import create_engine, Column, String, Integer, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 from models import *
-
 Base = declarative_base()
 
 class User(Base):
@@ -34,34 +33,59 @@ Base.metadata.create_all(engine)
 
 @app.route("/")
 def index():
-    #return render_template("login.html")
-    return render_template("signup.html")
+    return render_template("login.html", username_not_valid=False)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    input_username = request.form.get('username')
-    input_password = request.form.get('password')
-    user = db.query(User).filter(User.username==input_username and User.password==input_password).first()
-    if user is None:
-        return render_template("error.html", message="Could not find that user.")
+    if 'signup' in request.form:
+        return render_template("signup.html", username_exists=False)
     else:
-        return render_template("success.html")
+        input_username = request.form.get('username')
+        input_password = request.form.get('password')
+        user = db.query(User).filter(User.username==input_username and User.password==input_password).first()
+        if user is None:
+            return render_template("login.html", username_not_valid=True)
+        else:
+            current_user = user
+            return render_template("main.html", name=user.name)
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-
-    input_name = request.form.get('name')
-    input_username = request.form.get('username')
-    input_password = request.form.get('password')
-    user = db.query(User).filter(User.username==input_username).first()
-    if user is None:
-        user = User(name = input_name, username = input_username, password = input_password)
-        db.add(user)
-        db.commit()
-        return render_template("success.html")
+    if 'login' in request.form:
+        return render_template("login.html")
     else:
-        return render_template("error.html", message="Select a new username")
+        input_name = request.form.get('name')
+        input_username = request.form.get('username')
+        input_password = request.form.get('password')
+        user = db.query(User).filter(User.username==input_username).first()
+        if user is None:
+            user = User(name = input_name, username = input_username, password = input_password)
+            db.add(user)
+            db.commit()
+            return render_template("main.html")
+        else:
+            return render_template("signup.html", username_exists=True)
 
-@app.route("/success", methods=["POST"])
-def success():
-    return render_template("success.html")
+@app.route("/main", methods=["GET"])
+def main():
+    if 'search' in request.form:
+        key_word = request.form.get('key_word')
+        book = db.execute("SELECT * FROM books WHERE title LIKE '%:key_word%' OR author LIKE '%:key_word%' OR isbn LIKE '%:key_word%' OR year LIKE '%:key_word%'", 
+        {"key_word": key_word})
+        if book is not None:
+            return render_template("test.html", message=book.name)
+        else:
+            return render_template("test.html", message="None found!")
+    return render_template("main.html")
+
+@app.route("/profile", methods=["GET", "POST"])
+def profile():
+    if 'update' in request.form:
+        new_name = request.form.get('name')
+        user.name = new_name
+        db.commit()
+    return render_template("profile.html")
+
+@app.route("/test", methods=["GET", "POST"])
+def test():
+    return render_template("test.html", message="not working")
--- Book Review Web Site/models.py
+++ Book Review Web Site/models.py
@@ -4,13 +4,15 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Users(db.Model):
+class User(db.Model):
+    # Would be nice to add name and last name for the reviews
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String, nullable = False)
     username = db.Column(db.String, unique = True, nullable = False)
     password = db.Column(db.String, nullable = False)
 
-class Books(db.Model):
+class Book(db.Model):
     __tablename__ = "books"
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String, nullable = False)
@@ -20,7 +22,7 @@ class Books(db.Model):
     review_count = db.Column(db.Integer, nullable = False)
     average_score = db.Column(db.Integer, nullable = False)
 
-class Reviews(db.Model):
+class Review(db.Model):
     __tablename__ = "reviews"
     id = db.Column(db.Integer, primary_key = True)
     book = db.Column(db.Integer, db.ForeignKey("books.id"), nullable = False)
