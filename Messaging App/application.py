ec4e91f9685785a6d51330017c2b4cf7ec311aba
Alexandra <alexandragil98@outlook.com>
2020-06-24 22:48:23 -0700

    Messages are saved and can be uploaded again

--- Messaging App/application.py
+++ Messaging App/application.py
@@ -1,7 +1,6 @@
 import os
 from flask import Flask, session, redirect, render_template, request, jsonify
 from flask_socketio import SocketIO
-from flask_assets import Environment, Bundle
 from functools import wraps
 
 app = Flask(__name__)
@@ -9,14 +8,9 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 socketio.run(app, debug=True)
 
-assets = Environment(app)
-assets.url = app.static_url_path
-assets.debug = True
-
-scss = Bundle('sass/signin.scss', filters='pyscss', output='gen/all.css')
-assets.register('scss_all', scss)
-
-channels = { 1: "First", 2: "Second", 3:"Third"}
+# Associate channel name with list of messages, associate the dict and such with the session
+channels = {"first": [], "second":[], "third":[]}
+current_messages = []
 
 def login_required(f):
     """
@@ -32,7 +26,8 @@ def login_required(f):
 
 @app.route("/")
 def index():
-    return render_template("index.html", channels=channels)
+    print(channels)
+    return render_template("index.html", channels=channels.keys())
 
 @app.route("/signin", methods=['GET', 'POST'])
 def signin():
@@ -45,12 +40,31 @@ def signin():
     else:
         return render_template("signin.html")
 
-@app.route('/channel', methods=["POST", "GET"])
-def channel():
-    return render_template('channel.html')
+@app.route('/layout', methods=["GET"])
+def layout():
+    return render_template('layout.html')
+
+@app.route('/create', methods=["POST", "GET"])
+def create():
+    if request.method=='POST':
+        current_messages = []
+        key = request.form.get("name")
+        channels[key] = current_messages
+        return render_template('index.html', channels=channels.keys())
+    return render_template('create.html')
+
+
+@app.route('/channel/<string:name>', methods=["POST", "GET"])
+def channel(name):
+    current_messages = channels[name]
+    session["current_channel"] = name
+    return render_template('channel.html', messages = current_messages)
 
 @socketio.on('sending message')
 def send_message(json, methods=['GET', 'POST']):
     username = session["username"]
     message = json["message"]
+    current_messages.append([username, message])
+    channels[session["current_channel"]] = current_messages
+    print(current_messages)
     socketio.emit('message sent', {'username': username, 'message': message})
