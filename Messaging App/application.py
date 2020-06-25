637e7402fe8ef578d0420d548564e83273969c4c
Alexandra <alexandragil98@outlook.com>
2020-06-25 00:15:42 -0700

    Done!

--- Messaging App/application.py
+++ Messaging App/application.py
@@ -8,9 +8,9 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 socketio.run(app, debug=True)
 
-# Associate channel name with list of messages, associate the dict and such with the session
-channels = {"first": [], "second":[], "third":[]}
+channels = {}
 current_messages = []
+users = []
 
 def login_required(f):
     """
@@ -34,6 +34,8 @@ def signin():
     session.clear()
     if request.method == 'POST':
         username = request.form.get("display_name")
+        if username not in users:
+            users.append(username)
         session['username'] = username
         session.permanent = True
         return redirect("/")
@@ -44,27 +46,35 @@ def signin():
 def layout():
     return render_template('layout.html')
 
+@app.route('/logout')
+def logout():
+    session.clear()
+    return render_template('signin.html')
+
 @app.route('/create', methods=["POST", "GET"])
 def create():
     if request.method=='POST':
-        current_messages = []
         key = request.form.get("name")
-        channels[key] = current_messages
-        return render_template('index.html', channels=channels.keys())
-    return render_template('create.html')
+        if key in channels.keys():
+            return render_template('create.html', already_exists = True)
+        else:
+            current_messages = []
+            channels[key] = current_messages
+            return render_template('index.html', channels=channels.keys())
+    return render_template('create.html', already_exists = False)
 
 
 @app.route('/channel/<string:name>', methods=["POST", "GET"])
 def channel(name):
     current_messages = channels[name]
     session["current_channel"] = name
-    return render_template('channel.html', messages = current_messages)
+    return render_template('channel.html', messages = current_messages, name = name)
 
 @socketio.on('sending message')
 def send_message(json, methods=['GET', 'POST']):
     username = session["username"]
     message = json["message"]
-    current_messages.append([username, message])
+    timestamp = json["timestamp"]
+    current_messages.append([username, message, timestamp])
     channels[session["current_channel"]] = current_messages
-    print(current_messages)
-    socketio.emit('message sent', {'username': username, 'message': message})
+    socketio.emit('message sent', {'username': username, 'message': message, 'timestamp': timestamp})
